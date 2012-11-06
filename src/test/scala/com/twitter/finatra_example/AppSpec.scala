c7c9f7c6532a0eee39d890b494d27189ecc9bc5e
Julio Capote <julio@twitter.com>
2012-11-05 22:58:04 -0800

    updated example

--- src/test/scala/com/twitter/finatra_example/AppSpec.scala
+++ src/test/scala/com/twitter/finatra_example/AppSpec.scala
@@ -10,16 +10,34 @@ class AppSpec extends SpecHelper {
   val app = new App.ExampleApp
 
   
-  "GET /hello" should "response with hello world" in {
+  "GET /hello" should "respond with hello world" in {
     get("/hello")
     response.body should equal ("hello world")
-    response.code should equal (200)
   }
 
-  "GET /user/foo" should "response with foo" in {
+  "GET /user/foo" should "responsd with hello foo" in {
     get("/user/foo")
     response.body should equal ("hello foo")
-    response.code should equal (200)
+  }
+
+  "GET /headers" should "respond with Foo:Bar" in {
+    get("/headers")
+    response.getHeader("Foo") should equal("Bar")
+  }
+
+  "GET /data.json" should """respond with {"foo":"bar"}""" in {
+    get("/data.json")
+    response.body should equal("""{"foo":"bar"}""")
+  }
+
+  "GET /search?q=foo" should "respond with no results for foo" in {
+    get("/search?q=foo")
+    response.body should equal("no results for foo")
+  }
+
+  "GET /template" should "respond with a rendered template" in {
+    get("/template")
+    response.body should equal("Your value is random value here")
   }
 
 
